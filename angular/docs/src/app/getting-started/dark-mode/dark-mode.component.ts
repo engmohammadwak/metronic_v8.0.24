f4289b6b64b6a8fd23ba7bf2f895746f791a5c87
user <mwak.98@hotmail.com>
2024-06-10 19:11:24 +0400

    Initial commit

---
+++ angular/docs/src/app/getting-started/dark-mode/dark-mode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dark-mode',
+  templateUrl: './dark-mode.component.html',
+  styleUrls: ['./dark-mode.component.scss']
+})
+export class DarkModeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
